1af54d5abfd0cde50514d396b5a2cace4079af26
Shannon Moranetz <shannonrmoranetz@gmail.com>
2019-04-09 12:56:25 -0600

    Update tests and favicon

--- src/app/userpane/userpane.component.spec.ts
+++ src/app/userpane/userpane.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { UserpaneComponent } from './userpane.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('UserpaneComponent', () => {
   let component: UserpaneComponent;
@@ -10,7 +11,7 @@ describe('UserpaneComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserpaneComponent ],
-      imports: [ FormsModule, HttpClientTestingModule ]
+      imports: [ FormsModule, HttpClientTestingModule, FontAwesomeModule ]
     })
     .compileComponents();
   }));
